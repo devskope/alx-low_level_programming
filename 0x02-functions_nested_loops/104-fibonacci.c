616a6d858c5b7a23e69f17410e81340eb44b4f41
devskope <pmskope@gmail.com>
2022-12-28 14:32:45 +0100

    Completed task 16 - update readme - create 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,30 +4,44 @@
  * main - Program entry point
  * @void: no parameter
  *
- * Description: prints the sum of the first 98 fibonacci terms.
+ * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2.
  * followed by a new line.
  *
  * Return: 0
  */
 int main(void)
 {
-	unsigned long a = 0;
-	unsigned long b = 1;
-	unsigned long count;
+	unsigned long a, aDiv, aMod, b, bDiv, bMod, c, cDiv, cMod, count;
 
-	for (count = 1; count <= 98; count++)
+	a = 0;
+	b = 1;
+	c = a + b;
+
+	for (count = 1; count < 89; count++)
 	{
-		unsigned long term = a + b;
-
-		if (count == 98)
-			printf("%lu\n", term);
-		else
-		{
-			printf("%lu, ", term);
-			a = b;
-			b = term;
-		}
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
 	}
+
+	bDiv = b / 1000000000;
+	bMod = b % 1000000000;
+	cDiv = c / 1000000000;
+	cMod = c % 1000000000;
+
+	for (count = 89; count < 98; count++)
+	{
+		printf("%lu%lu, ", cDiv, cMod);
+		aDiv = bDiv;
+		aMod = bMod;
+		bDiv = cDiv;
+		bMod = cMod;
+		cDiv = aDiv + bDiv + ((aMod + bMod) / 1000000000);
+		cMod = (aMod + bMod) % 1000000000;
+	}
+
+	printf("%lu%lu\n", cDiv, cMod);
 
 	return (0);
 }
